4d47f4709d9dcde6311c16d3c7babc718e71a2ff
jerrod albright <ultspider@gmail.com>
2019-08-06 19:42:25 -0500

    Maybe?

---
+++ Hiya.js
@@ -0,0 +1,23 @@
+const name = "hello";
+const numbo = 5;
+const list = [1, 3, 5]
+const testObj = {
+  name: "jerrod",
+  age: 16,
+  favoriteFood: "dogs"
+}
+function testFunc() {
+  
+}
+
+
+const testFunc2 = (age, name) => {
+  console.log("I am " + name + " and my age is " + age)
+}
+
+testFunc2(3, "Steve")
+
+console.log(testObj.name);
+testObj.age = 24;
+testObj.foobar = "asdfa";
+list[0]
